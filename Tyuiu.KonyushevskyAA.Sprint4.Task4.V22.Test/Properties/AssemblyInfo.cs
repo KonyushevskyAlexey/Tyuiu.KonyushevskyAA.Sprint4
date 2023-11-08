1e33b6c22a35b8f5ed65981fdb2710cc31600f53
Tyuiu.KonyushevskyAA@outlook.com <aleksej.konyushevskij@mail.ru>
2023-11-08 19:22:25 +0500

    Task4

---
+++ Tyuiu.KonyushevskyAA.Sprint4.Task4.V22.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KonyushevskyAA.Sprint4.Task4.V22.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KonyushevskyAA.Sprint4.Task4.V22.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5c1b9630-b9b8-47a8-a4eb-e40f755b1057")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.KonyushevskyAA.Sprint4.Task4.V22/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.KonyushevskyAA.Sprint4.Task4.V22.Lib;
+
+namespace Tyuiu.KonyushevskyAA.Sprint4.Task4.V22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #4 | Выполнил: Конюшевский А.А   | ИИПБ-23-2";
+            Console.WriteLine("*******************************************************************************************************************");
+            Console.WriteLine("* Спринт #4                                                                                                       *");
+            Console.WriteLine("* Тема: Двумерные массивы. (ввод с клавиатуры)                                                                    *");
+            Console.WriteLine("* Задание #4                                                                                                      *");
+            Console.WriteLine("* Вариант #22                                                                                                     *");
+            Console.WriteLine("* Выполнил: Конюшевский Алексей Александрович  | ИИПБ-23-2                                                        *");
+            Console.WriteLine("*******************************************************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                                                        *");
+            Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов, заполненный статическими значениями в диапазоне от 2 до 6. *");
+            Console.WriteLine("* Заменить нечетные элементы массива на 0.                                                                        *");
+            Console.WriteLine("* 4, 3, 2, 4, 5,                                                                                                  *");
+            Console.WriteLine("* 5, 4, 5, 6, 6,                                                                                                  *");
+            Console.WriteLine("* 6, 2, 3, 6, 5,                                                                                                  *");
+            Console.WriteLine("* 6, 2, 3, 6, 4,                                                                                                  *");
+            Console.WriteLine("* 6, 2, 6, 6, 2,                                                                                                  *");
+            Console.WriteLine("*******************************************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                                                *");
+            Console.WriteLine("*******************************************************************************************************************");
+
+            Console.WriteLine("Введите количество строк массива: ");
+            int rows = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите количество строк массива: ");
+            int columns = Convert.ToInt32(Console.ReadLine());
+
+            int[,] matrix = new int[rows, columns];
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.WriteLine($"Введите {i},{j} элемент массива");
+                    matrix[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("\nМассив:");
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{matrix[i, j]} \t");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                               *");
+            Console.WriteLine("****************************************************************************");
+
+            int[,] res = ds.Calculate(matrix);
+
+            Console.WriteLine("\nИзмененный массив:");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{res[i, j]} \t");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        
+        }
+    }
+}
